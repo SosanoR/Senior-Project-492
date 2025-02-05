149450908baafb261c07999350ddab424d06616b
Ricardo Hernandez <59302916+SosanoR@users.noreply.github.com>
2025-02-05 15:51:48 -0800

    add header, footer, and theme toggle components; update layout and constants

---
+++ ecom492/lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME =  process.env.NEXT_PUBLIC_APP_NAME || "Mercury Express";
+export const APP_DESCRIPTION =  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "An Ecommerce Project creating using Nextjs by Ricardo Hernandez";
+export const SERVER_URL =  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
